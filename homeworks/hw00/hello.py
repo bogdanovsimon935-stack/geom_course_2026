96c4704d03c525153287766c523ff8d6c312fab3
Богданов Семен <bogdanovsimon935@gmail.com>
2026-09-04 23:45:30 +0300

    Complete homework 0

---
+++ homeworks/hw00/hello.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+
+
+print(f"Версия Python: {sys.version.split()[0]}")
+print(f"Версия NumPy: {np.__version__}")
+print(f"Версия Pandas: {pd.__version__}")
+print(f"Версия Matplotlib: {matplotlib.__version__}")
